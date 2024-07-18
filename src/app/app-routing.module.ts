6896ef86bdb3ea169e8ccbef67fb6cbcb3b704d6
Gopi Katari <katarigopi@gmail.com>
2024-07-18 22:06:10 +0530

    routing example

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/routing/home/home.component';
+import { AboutComponent } from './components/routing/about/about.component';
+import { ContactComponent } from './components/routing/contact/contact.component';
 
-const routes: Routes = [];
+//step 1 define you r compontes in the routes array below
+//step 2 use the path variable in the navigation
+//step 3 define router-outlet ( will tell you where exactry the requested route content to be displayed)
+//step 4 add RouterLink atrribute
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  {
+    path: 'about',
+    component: AboutComponent,
+  },
+  {
+    path: 'phani',
+    component: ContactComponent,
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'testapp';
-
+  public dateTime: string = new Date().toString();
   public imgURL: string =
     'https://spn-sta.spinny.com/blog/20221004191046/Hyundai-Venue-2022.jpg?compress=true&quality=80&w=1200&dpr=2.6';
+
+  public getCurrentTime(): string {
+    setInterval(() => {
+      this.dateTime = new Date().toString();
+    }, 1000);
+    return this.dateTime;
+  }
 }
